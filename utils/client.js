7a8a0c7b71a137b49647583bd05043a774e64608
Ariel_Naiman <a0527633600@gmail.com>
2025-12-04 10:18:57 +0200

    finish client.js

---
+++ utils/client.js
@@ -0,0 +1,25 @@
+import input from 'analiza-sync';
+import all from '../riddles/all.js';
+
+let id = 0
+
+export function name() {
+    return input("What is your name? ")
+}
+
+export function choice() {
+    return input("Select the desired option: ")
+}
+
+export function add_puzzle() {
+    return {
+        id: (all.length + 1),
+        name: input("Enter the name of the puzzle: "),
+        taskDescription: input("Enter the riddle: "),
+        correctAnswer: input("Enter the answer: ")
+    }
+}
+
+export function id_puzzle() {
+    return input("Enter the requested ID: ")
+}
