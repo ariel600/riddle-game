b1848b073a33a1a12adbc36db251065a7eecaff3
Ariel_Naiman <a0527633600@gmail.com>
2025-12-04 10:23:32 +0200

    finish client.js

--- utils/client.js
+++ utils/client.js
@@ -11,15 +11,15 @@ export function choice() {
     return input("Select the desired option: ")
 }
 
-export function add_puzzle() {
+export function add_riddle() {
     return {
         id: (all.length + 1),
-        name: input("Enter the name of the puzzle: "),
+        name: input("Enter the name of the riddle: "),
         taskDescription: input("Enter the riddle: "),
         correctAnswer: input("Enter the answer: ")
     }
 }
 
-export function id_puzzle() {
+export function id_riddle() {
     return input("Enter the requested ID: ")
 }
